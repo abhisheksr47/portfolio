538740dcca9f5af5fe9fe57ea2756c09c7dfad27
Abhishek S R <66238457+abhishek123-asr@users.noreply.github.com>
2024-10-15 14:33:16 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
 
 const hero_content = {
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+  text: "Innovator at the intersection of technology and design, with a passion for pushing creative boundaries. My journey has led me through the realms of DevOps, cloud architecture, machine learning, and user experience design. As a freelance professional, I've honed my skills in both graphic design and software development, crafting bespoke solutions for diverse clients."
 };
 
 const container = (delay) => ({
