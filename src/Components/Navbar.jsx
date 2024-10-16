f8af2d2d0f051fa6a0ca55bc1be18234b77bdd52
Abhishek S R <66238457+abhisheksr47@users.noreply.github.com>
2024-10-16 12:12:16 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,11 +18,11 @@ function Navbar() {
         </a>
         {/* Cooler Download CV button */}
         <a href="https://drive.google.com/uc?export=download&id=1cOWB-Lc5N1VvoTQhq0fFRYtd-891QfQM">
-          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-            <span className="relative px-4 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-xs md:px-5 md:py-2.5 md:text-sm">
-              Download CV
-            </span>
-          </button>
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 bg-white rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 dark:bg-gray-900">
+  <span className="relative px-4 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-xs md:px-5 md:py-2.5 md:text-sm">
+    Download CV
+  </span>
+</button>
         </a>
       </div>
     </nav>
