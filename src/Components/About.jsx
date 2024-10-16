433b2086e5ca08d97c9c32ce5b7cc1095e5769dc
Abhishek S R <abhisheksr47@gmail.com>
2024-10-16 07:32:32 +0530

    fix download about animation

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -19,19 +19,20 @@ function About() {
         <span className='text-neutral-500'> Me</span>
       </motion.h1>
       
-      <div className='flex flex-wrap justify-between'>
+      <div className='flex flex-wrap'>
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: -100 }}
           transition={{ duration: 0.5 }}
-          className='w-full lg:w-1/2 lg:p-8 flex items-center justify-center'
+          className='w-full lg:w-1/2 lg:p-8'
         >
-          <img 
-            src={aboutImage} 
-            alt="About Me" 
-            className='rounded-2xl max-h-full' 
-            style={{ objectFit: 'cover', maxHeight: '100%' }} 
-          />
+          <div className='flex items-center justify-center'>
+            <img 
+              src={aboutImage} 
+              alt="About Me" 
+              className='rounded-2xl' 
+            />
+          </div>
         </motion.div>
 
         <motion.div
